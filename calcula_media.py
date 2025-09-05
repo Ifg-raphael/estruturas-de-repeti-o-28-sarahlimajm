45af1e25464af51fcf9b96bf933782cec887c413
sarahlimajm <sarahstolima@gmail.com>
2025-09-05 17:23:52 -0300

    exercicio

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,11 @@
 # Sua solução aqui
+#Criação de cinco váriaveis para armazenar os valor que serão inseridos, respectivamente.
+n1 = int(input("Informe cinco números: "))
+n2 = int(input(" "))
+n3 = int(input(" "))
+n4 = int(input(" "))
+n5 = int(input(" "))
+#Criação de uma váriavel que é representada por uma função.
+media = (n1 + n2 + n3 + n4 + n5) /5
+#Imprime o valor da media.
+print(f"{media:.2f}")
